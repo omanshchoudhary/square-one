73cf8ce093ec24921600a79ea139a49bb6f0b02e
Omansh Choudhary <omansh.choudhary@gmail.com>
2026-08-29 18:29:21 +0530

    unix-shell: feat: add stdin and stdout redirection

--- unix-shell/src/parser.rs
+++ unix-shell/src/parser.rs
@@ -1,13 +1,40 @@
 pub struct Argv {
     pub program: String,
     pub args: Vec<String>,
+    pub stdout: Option<(String, bool)>,
+    pub stdin: Option<String>,
 }
 
 impl Argv {
     pub fn new(line: String) -> Option<Self> {
-        let mut parts = line.split_whitespace();
-        let program = parts.next()?.to_string();
-        let args = parts.map(str::to_string).collect();
-        Some(Self { program, args })
+        let mut tokens = line.split_whitespace().peekable();
+        let mut words: Vec<String> = Vec::new();
+        let mut stdin = None;
+        let mut stdout = None;
+
+        while let Some(tok) = tokens.next() {
+            match tok {
+                "<" | ">" | ">>" => {
+                    let path = tokens.next()?.to_string();
+                    match tok {
+                        "<" => stdin = Some(path),
+                        ">" => stdout = Some((path, false)),
+                        ">>" => stdout = Some((path, true)),
+                        _ => unreachable!(),
+                    }
+                }
+                _ => words.push(tok.to_string()),
+            }
+        }
+
+        let mut words = words.into_iter();
+        let program = words.next()?;
+        let args = words.collect();
+        Some(Self {
+            program,
+            args,
+            stdout,
+            stdin,
+        })
     }
 }
--- unix-shell/src/process.rs
+++ unix-shell/src/process.rs
@@ -1,16 +1,18 @@
 use crate::parser::Argv;
 
 use std::ffi::CString;
-use nix::unistd::{fork, execvp, ForkResult};
+use nix::fcntl::{open, OFlag};
+use nix::sys::stat::Mode;
 use nix::sys::wait::waitpid;
+use nix::unistd::{dup2_stdin, dup2_stdout, execvp, fork, ForkResult};
 
 pub fn run(unit: Argv) {
-    let file = CString::new(unit.program).unwrap();
+    let file = CString::new(unit.program.as_str()).unwrap();
     let mut argv = vec![file.clone()];
     argv.extend(
         unit.args
-            .into_iter()
-            .map(|arg| CString::new(arg).unwrap()),
+            .iter()
+            .map(|arg| CString::new(arg.as_str()).unwrap()),
     );
 
     match unsafe { fork() } {
@@ -18,9 +20,32 @@ pub fn run(unit: Argv) {
             waitpid(child, None).unwrap();
         }
         Ok(ForkResult::Child) => {
+            if setup_redirects(&unit).is_err() {
+                unsafe { nix::libc::_exit(1) };
+            }
             let _ = execvp(&file, &argv);
             unsafe { nix::libc::_exit(1) };
         }
         Err(e) => eprintln!("fork: {e}"),
     }
+}
+
+fn setup_redirects(unit: &Argv) -> nix::Result<()> {
+    if let Some(path) = &unit.stdin {
+        let fd = open(path.as_str(), OFlag::O_RDONLY, Mode::empty())?;
+        dup2_stdin(&fd)?;
+    }
+
+    if let Some((path, append)) = &unit.stdout {
+        let mut flags = OFlag::O_WRONLY | OFlag::O_CREAT;
+        flags |= if *append {
+            OFlag::O_APPEND
+        } else {
+            OFlag::O_TRUNC
+        };
+        let fd = open(path.as_str(), flags, Mode::from_bits_truncate(0o644))?;
+        dup2_stdout(&fd)?;
+    }
+
+    Ok(())
 }
